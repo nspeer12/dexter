8d01245b7a95173bef911ec7e31f9abc893b3dc8
Nick <nspeer252@gmail.com>
2021-02-10 12:41:37 -0500

    wake word

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import warnings
 import wikipedia
 
 MIC_SOURCE = 2
+WAKE_WORD = "Dexter"
 
 def get_voices():
 	voices = engine.getProperty('voices')
@@ -18,6 +19,31 @@ def get_voices():
 		engine.stop()
 
 
+def listen_for_wake():
+	sleep = True
+
+	while sleep:
+		with sr.Microphone(MIC_SOURCE) as source:
+			print("Adjusting noise ")
+			recognizer.adjust_for_ambient_noise(source, duration=1)
+			print("Waiting for wake word")
+			recorded_audio = recognizer.listen(source)
+			print("Done recording")
+		try:
+			print("Recognizing the text")
+			text = recognizer.recognize_google(
+					recorded_audio, 
+					language="en-US"
+				)
+			print("Decoded Text : {}".format(text))
+
+			if WAKE_WORD in text:
+				print("AWOKEN")
+				speak("how can I help you")
+				sleep = False
+
+		except Exception as ex:
+			print(ex)
 
 def recordAudio():
 	with sr.Microphone(MIC_SOURCE) as source:
@@ -38,6 +64,11 @@ def recordAudio():
 		print(ex)
 
 
+def speak(text:str):
+	engine.say(text)
+	engine.runAndWait()
+
+
 if __name__ == '__main__':
 	engine = pyttsx3.init()
 	recognizer = sr.Recognizer()
@@ -47,4 +78,4 @@ if __name__ == '__main__':
 	engine.say("My name is Dexter")
 	engine.runAndWait()
 
-	recordAudio()
+	listen_for_wake()
