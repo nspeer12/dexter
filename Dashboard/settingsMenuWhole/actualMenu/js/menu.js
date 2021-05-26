f42f75fb016f2d73eeb02c01c07b238f3a8da302
Eris <fitzgeralderis@gmail.com>
2021-05-26 16:51:45 -0400

    updated all settings menu files to the status they were for the presentation

--- Dashboard/settingsMenuWhole/actualMenu/js/menu.js
+++ Dashboard/settingsMenuWhole/actualMenu/js/menu.js
@@ -118,10 +118,15 @@ $(document).ready(function(){
 
   });
 
-
-
-
 });
+
+$(window).scroll(function(e){
+  parallax();
+});
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.bg').css('top',-(scrolled*0.2)+'px');
+}
 
 
 
