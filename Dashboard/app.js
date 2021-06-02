67b801d945ac501f52d88baeb9b3da0e7924b0a1
Cameron Lindsey <cameronlindsey@knights.ucf.edu>
2021-06-01 20:24:30 -0400

    Fixed bug in app.js

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -29,7 +29,7 @@ function createWindow() {
     })
 
     // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/Pages/Home.html');
+    mainWindow.loadURL('file://' + __dirname + '/Pages/Dashboard.html');
     //mainWindow.webContents.openDevTools()
     
     
