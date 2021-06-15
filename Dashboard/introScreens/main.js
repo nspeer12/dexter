d9f83427402858d467f6199ba6a325541021c649
Eris <fitzgeralderis@gmail.com>
2021-06-15 15:41:09 -0400

    gesture menu and intro set up

--- Dashboard/introScreens/main.js
+++ Dashboard/introScreens/main.js
@@ -8,7 +8,17 @@ function createWindow () {
     win.loadFile('introScreen.html')
   }
   
+  
   app.whenReady().then(() => {
     createWindow()
   })
+  
+  $(document).ready(function(){
+    $("ul.nav li a[href^='#']").click(function(){
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 400);
+    });
+});
+  
 
